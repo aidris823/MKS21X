cc7f8669ab902241db632b45d5a9ec3cda464835
[pokeanand@gmail.com] <pokeanand@gmail.com>
2017-12-05 22:21:37 -0500

    Added newer Barcode file and created 08Sorts file.

--- 07BarCode/BarCode.java
+++ 07BarCode/BarCode.java
@@ -1,53 +1,165 @@
-public class BarCode{
+
+public class Barcode {
+    //The *nuclear* codes:
     private String[] codes = {"||:::",
-			    ":::||",
-			    "::|:|",
-			    "::||:",
-			    ":|::|",
-			    ":|:|:",
-			    ":||::",
-			    "|:::|",
-			    "|::|:",
-			    "|:|::"};
-    //You technically don't need a Barcode String because ZipCode can calculate that.
+                ":::||",
+                "::|:|",
+                "::||:",
+                ":|::|",
+                ":|:|:",
+                ":||::",
+                "|:::|",
+                "|::|:",
+                "|:|::"};
+    //You don't need a code String because Zipcode can calculate that.  Therefore, we just have a zip.
     private String zip;
-    //I chose to make one anyway.:
-    private String barcode = "|";
     //Constructor:
-    public BarCode(String z){
-	zip = z;
-	int sum = 0;
-	for (int i = 0; i < z.length(); i++){
-	    sum += Integer.parseInt(z.substring(i,i+1));
-	}
-	sum %= 10;
-	zip += sum;
-	for (int i = 0; i < z.length(); i++){
-	    barcode += codes[Integer.parseInt(z.substring(i,i+1))];
-	}
-	barcode += "|";
+    public Barcode(String _zip) {
+        if (_zip.length() != 5) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < _zip.length(); i++) {
+            if (!Character.isDigit(_zip.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+        zip = _zip;
     }
-    public String toString(){
-	return barcode + " (" + zip + ")";
+    //To String function; Returns "[code + check number] (zip)"
+    public String toString() {
+        String ans = "|";
+        for (int i = 0; i < zip.length(); i++) {
+            ans += codes[Integer.parseInt(zip.substring(i,i+1))];
+        }
+        int check = 0;
+        for (int i = 0; i < zip.length(); i++) {
+            check += Integer.parseInt(zip.substring(i,i+1));
+        }
+        check %= 10;
+        ans += codes[check] + "|" + " (" + zip + ")";
+        return ans;
     }
-    public String getZip(){
-	return zip.substring(0,zip.length()-2);
+    //Compares objects with class Barcode by their numerical order.
+    public int compareTo(Barcode other) {
+        return (this.zip).compareTo(other.getZip());
+    }
+    //Static function that don't need no outside zip; converts a 5 digit zipcode to code.
+    public static String toCode(String zip){
+        String[] codes = {"||:::",
+                ":::||",
+                "::|:|",
+                "::||:",
+                ":|::|",
+                ":|:|:",
+                ":||::",
+                "|:::|",
+                "|::|:",
+                "|:|::"};
+        if (zip.length() != 5) {
+            System.out.println(""+zip.length());
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < zip.length(); i++) {
+            if (!Character.isDigit(zip.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+        String ans = "|";
+        for (int i = 0; i < zip.length(); i++) {
+            ans += codes[Integer.parseInt(zip.substring(i,i+1))];
+        }
+        int check = 0;
+        for (int i = 0; i < zip.length(); i++) {
+            check += Integer.parseInt(zip.substring(i,i+1));
+        }
+        check %= 10;
+        ans += codes[check] + "|";
+        zip = ans;
+        return zip;
+    }
+    //Throws IllegalArgumentException if checksum is not valid, the sequence is not valid, non-barcode characters are in use, barcode's length is not 32, or if the left and rightmost characters aren't "|".
+    public static String toZip(String code) {
+        String rawCode = new String(code.substring(1, code.length() - 6));
+        String zip = "";
+
+        String[] codes = {"||:::",
+                ":::||",
+                "::|:|",
+                "::||:",
+                ":|::|",
+                ":|:|:",
+                ":||::",
+                "|:::|",
+                "|::|:",
+                "|:|::"};
+
+        if (code.length() != 32 || code.charAt(0) != '|' || code.charAt(code.length() - 1) != '|') {
+            throw new IllegalArgumentException();
+        }
+
+
+        for (int i = 0; i < code.length()-1; i++){
+            if ((code.charAt(i) != ':') && (code.charAt(i) != '|')){
+                throw new IllegalArgumentException();
+            }
+        }
+
+
+        for (int i = 0; i < rawCode.length(); i += 5){
+            if ((rawCode.substring(i,i+5).equals(codes[0])) && (rawCode.substring(i,i+5).equals(codes[1])) && (rawCode.substring(i,i+5).equals(codes[2])) && (rawCode.substring(i,i+5).equals(codes[3])) &&
+                    (rawCode.substring(i,i+5).equals(codes[4])) && (rawCode.substring(i,i+5).equals(codes[5])) && (rawCode.substring(i,i+5).equals(codes[6])) && (rawCode.substring(i,i+5).equals(codes[7])) &&
+                    (rawCode.substring(i,i+5).equals(codes[8])) && rawCode.substring(i,i+5).equals(codes[9])){
+                throw new IllegalArgumentException();
+            }
+        }
+
+        for (int i = 0; i < rawCode.length(); i += 5){
+            for (int j = 0; j < codes.length; j++){
+                if ((rawCode.substring(i,i+5).equals(codes[j]))){
+                    zip += j;
+                }
+            }
+        }
+        return zip;
+
     }
     public String getCode(){
-	return barcode;
+        return toCode(zip);
     }
-    public boolean equals(BarCode b){
-	return (compareTo(b) == 0);
+    public String getZip(){
+        return zip;
     }
-    public int compareTo(BarCode b){
-	return (this.barcode).compareTo(b.getCode());
+    public boolean equals(Barcode b) {
+        return (compareTo(b) == 0);
     }
+
     public static void main(String[] arguments){
-	String qwerty = "125358";
-	BarCode x = new BarCode(qwerty);
-	System.out.println(x.getZip());
-	System.out.println(x.getCode());
-    }
-}
-    
-				    
+       // Barcode b = new Barcode("92010");
+      //  System.out.println(b.toString());
+        //I used Jason Lin's Driver to test my function here :
+        Barcode a = new Barcode("11245");
+        Barcode b = new Barcode("38760");
+        Barcode c = new Barcode("11245");
+        Barcode d = new Barcode("00294");
+
+        System.out.println(a.getZip());//11245
+        System.out.println(d.getZip());//00234
+        System.out.println(a.getCode());// |:::||:::||::|:|:|::|:|:|:::||:|
+        System.out.println(a.toString());// |:::||:::||::|:|:|::|:|:|:::||:| (11245)
+        System.out.println(b);// |::||:|::|:|:::|:||::||::::|::|| (38760)
+        System.out.println(c);// |:::||:::||::|:|:|::|:|:|:::||:| (11245)
+        System.out.println(d);// |||:::||:::::|:||:|:::|::|:|:|:| (00294)
+        System.out.println(a.compareTo(b));// negative
+        System.out.println(a.compareTo(c));// 0
+        System.out.println(a.compareTo(d));// positive
+        System.out.println(a.equals(b));// false
+        System.out.println(a.equals(c));// true
+        System.out.println(a.equals(d));// false
+
+        //My own tests;
+        System.out.println(a.toZip("|:::||:::||::|:|:|::|:|:|:::||:|")); // Expected: 11245
+       System.out.println(a.toCode("11245")); //Expected: |:::||:::||::|:|:|::|:|:|:::||:|
+     //   System.out.println(a.toZip("|||||::::::::::::::::::::::::::::::::::::::")); //Expected: Exception
+      //  System.out.println(a.toCode("12345")); //Expected: Exception
+            }
+        }
