a6902668e567ead3b247c27e18af9e9b925387f6
Anand Idris <30372144+aidris823@users.noreply.github.com>
2018-01-06 18:25:41 -0500

    Added Book.java file...oops.
    This was uploaded through Github online rather than through Git, as my desktop (how I normally use Git) is currently at BestBuy for repair =(
    
    I feel like a caveman by uploading through this.

---
+++ 05Library/Book.java
@@ -0,0 +1,53 @@
+public class Book{
+ //The file with my original Book file was on my Desktop, so I had to rewrite this file.  
+ //Luckily the info of what we had to write was on the website (posted on November 28th, 2017) and it was not very long code.
+ 
+ //Fields:
+  private String author;
+  private String title;
+  private String ISBN;
+  
+  //Null constructor:
+  public Book(){
+    author = "";
+    title = "";
+    isbn = "";
+  }
+  
+  //Constructor for author, title, and ISBN:
+  public Book(String a, String t, String isbn){
+    author = a;
+    title = t;
+    ISBN = isbn;
+ 
+ //Getters:
+  public String getAuthor(){
+    return author;
+  }
+  public String getTitle(){
+    return title;
+  }
+  public String getISBN(){
+   return ISBN;
+ }
+ 
+ //Setters: 
+  public void setAuthor(String a){
+    author = a;
+    }
+ public void setTitle(String t){
+  title = t;
+  }
+  public void setISBN(String isbn){
+    ISBN = isbn;
+  }
+  
+  //ToString in "Title, Author, ISBN" format
+  public String toString(){
+    String ans = "" + title + "," + author + "," + ISBN;
+    return ans;
+  }
+    
+ }
+ 
+  
